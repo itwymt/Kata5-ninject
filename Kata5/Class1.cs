81abd83878d49e03afc97262d342988c526498da
unknown <heleng@mak007.compatibl.com>
2012-09-19 15:33:23 +0300

    Kata5 project

---
+++ Kata5/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace Kata5
+{
+    public delegate void WriteEventHandler(object sender, WriteEventArgs args);
+
+    public class WriteEventArgs
+    {
+        private readonly String _text;
+
+        public WriteEventArgs(string text)
+        {
+            _text = text;
+        }
+
+        public string GetText()
+        {
+            return _text.Clone().ToString();
+        }
+    }
+
+    public sealed class Publisher: IPublisher
+    {
+        private readonly TextReader _textReader;
+
+        public event WriteEventHandler WriteEvent;
+
+        
+
+        public Publisher(TextReader textReader)
+        {
+            _textReader = textReader;
+        }
+
+        private void RaiceWriteEvent(WriteEventArgs eventArgs)
+        {
+            if (WriteEvent!=null)
+            {
+                WriteEvent(this, eventArgs);
+            }
+        }
+
+        public void ReadText()
+        {
+            string s;
+            while ((s = _textReader.ReadLine()) != null)
+            {
+                if (!String.IsNullOrEmpty(s))
+                {
+                    RaiceWriteEvent(new WriteEventArgs(s));
+                }
+            }
+        }
+    }
+
+    public class Subscriber
+    {
+        private readonly TextWriter _textWriter;
+
+        public Subscriber(IPublisher publisher, TextWriter textWriter)
+        {
+            _textWriter = textWriter;
+
+            if (publisher != null) 
+                publisher.WriteEvent += HandleWriteEvent;
+        }
+
+        private void HandleWriteEvent(object sender, WriteEventArgs eventArgs)
+        {
+            WriteText(eventArgs.GetText());
+        }
+
+        private void WriteText(string s)
+        {
+            _textWriter.WriteLine(s);
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var publisher = new Publisher(Console.In);
+            var subsriber = new Subscriber(publisher, Console.Out);
+
+            publisher.ReadText();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kata5/Tests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace Kata5
+{
+    public class TestPablisher
+    {
+        private string readedText;
+        [Fact]
+        public void test_read_text()
+        {
+            readedText = "";
+            var str = new [] {"lhyiuyo","ddddd","dddddd"};
+            var tr = new StringReader(string.Join(",", str));
+            var publisher = new Publisher(tr);
+            publisher.WriteEvent += HandlePublisherEvent;
+            publisher.ReadText();
+            readedText.Should().Be("lhyiuyo,ddddd,dddddd");
+        }
+
+        [Fact]
+        public void test_read_text1()
+        {
+            readedText = "";
+            var str = new[] { "lhyiuyo", "ddddd", "1234qwere" };
+            var tr = new StringReader(string.Join("\n", str));
+            var publisher = new Publisher(tr);
+            publisher.WriteEvent += HandlePublisherEvent;
+            publisher.ReadText();
+            readedText.Should().Be("lhyiuyo,ddddd,1234qwere");
+        }
+
+        private void HandlePublisherEvent(Object s, WriteEventArgs eventArgs)
+        {
+            if (readedText != "")
+            {
+                readedText += ",";
+            }
+            readedText += eventArgs.GetText();
+        }
+    }
+
+    public class TestSubscriber
+    {
+        [Fact]
+        public void test_subsriber()
+        {
+            var tp = new TestPublisher();
+            var sb = new StringBuilder();
+            var tw = new StringWriter(sb);
+            var subscriber = new Subscriber(tp, tw);
+            tp.Raise("abcd");
+            sb.ToString().Should().Be("abcd\r\n");
+        }
+
+        [Fact]
+        public void test_subsriber1()
+        {
+            var tp = new TestPublisher();
+            var sb = new StringBuilder();
+            var tw = new StringWriter(sb);
+            var subscriber = new Subscriber(tp, tw);
+            tp.Raise("");
+            sb.ToString().Should().Be("\r\n");
+        }
+    }
+
+    public class TestPublisher : IPublisher
+    {
+        public event WriteEventHandler WriteEvent;
+        public void Raise(string s)
+        {
+            var eventArgs = new WriteEventArgs(s);
+            WriteEvent(this, eventArgs);
+        }
+    }
+}
